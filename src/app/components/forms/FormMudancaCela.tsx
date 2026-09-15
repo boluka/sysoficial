f316919a536312ae72cf9fee74ea70d26f362bb7
Lucas Borgo Soares <lsoares@lbtech.local>
2026-09-15 18:12:04 -0400

    Trabalhando a lógica do FormMudancaCela

--- src/app/components/forms/FormMudancaCela.tsx
+++ src/app/components/forms/FormMudancaCela.tsx
@@ -1,23 +1,68 @@
-'use client";'
+'use client";';
 import InputComponentForm from "../input/InputComponentForm";
 import FormDefault from "./FormDefault";
 import styles from "./styles-forms.module.css";
 import SelectComponentForm from "../select/Select";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface Setores {
+  pav: string;
+  sol: string;
+  cela: string;
+}
+
+interface FormMudancaState {
+  1: {de: Setores, para: Setores};
+  2: {de: Setores, para: Setores}
+}
 
 export default function FormMudancaCela() {
-    const [data, setData] = useState({
-        'de': {
-            'pav': '',
-            'sol': '',
-            'cela': '',
-        },
-        'para': {
-            'pav': '',
-            'sol': '',
-            'cela': '',
-        }
-    });
+  const [data, setData] = useState<FormMudancaState>({
+    "1": {
+      de: {
+        pav: "",
+        sol: "",
+        cela: "",
+      },
+      para: {
+        pav: "",
+        sol: "",
+        cela: "",
+      }
+    },
+    "2": {
+      de: {
+        pav: "",
+        sol: "",
+        cela: "",
+      },
+      para: {
+        pav: "",
+        sol: "",
+        cela: "",
+      }
+    },
+  });
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+
+    const [numero, bloco , campo] = name.split(".");
+
+    const blocoValido = bloco as keyof FormMudancaState;
+
+    setData((prev) => ({
+      ...prev,
+      [numero]: {
+        ...prev[blocoValido],
+        [bloco] : {
+          ...prev[blocoValido]
+        } 
+      },
+    }));
+  };
   return (
     <FormDefault>
       <h2 className="text-center bg-cinza-escuro rounded-[5px] text-amarelo-claro text-[1.3em] font-bold">
@@ -32,39 +77,284 @@ export default function FormMudancaCela() {
           <fieldset className="flex flex-col grow gap-3 border border-amarelo-claro rounded-[5px] p-2">
             <legend className=" text-amarelo-claro text-center">De:</legend>
             <div className="flex justify-center gap-2">
-                <p>PAV:</p>
-              <SelectComponentForm type="text" width="w-[80%]">
-                {['I', 'II'].map((e) => {
-                  return <option className={`${styles['style-opt']}`} key={e} value={e}>{e}</option>;
+              <p>PAV:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="1.de.pav"
+              >
+                {["", "I", "II"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
                 })}
               </SelectComponentForm>
             </div>
             <div className="flex justify-center gap-2">
-                <p>SOL:</p>
-               <SelectComponentForm type="text" width="w-[80%]">
-                {['1A', '2A', '3A', '1B', '2B', '3B', 'DISC', 'ALA G'].map((e) => {
-                  return <option className={`${styles['style-opt']}`} key={e} value={e}>{e}</option>;
-                })}
+              <p>SOL:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="de.sol"
+              >
+                {["", "1A", "2A", "3A", "1B", "2B", "3B", "DISC", "ALA G"].map(
+                  (e) => {
+                    return (
+                      <option
+                        className={`${styles["style-opt"]}`}
+                        key={e}
+                        value={e}
+                      >
+                        {e === "" ? "Selecione uma opção..." : e}
+                      </option>
+                    );
+                  },
+                )}
               </SelectComponentForm>
             </div>
             <div className="flex justify-center gap-2">
-                <p>CELA:</p>
-              <SelectComponentForm type="text" width="w-[80%]" />
+              <p>CELA:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="de.cela"
+              >
+                {["", "1", "2", "3", "4", "5", "6"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
+                })}
+              </SelectComponentForm>
             </div>
           </fieldset>
           <fieldset className="flex flex-col grow gap-3 border border-amarelo-claro rounded-[5px] p-2">
             <legend className=" text-amarelo-claro text-center">Para:</legend>
             <div className="flex justify-center gap-2">
-                <p>PAV:</p>
-              <SelectComponentForm type="text" width="w-[80%]" />
+              <p>PAV:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="para.pav"
+              >
+                {["", "I", "II"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
+                })}
+              </SelectComponentForm>
             </div>
             <div className="flex justify-center gap-2">
-                <p>SOL:</p>
-              <SelectComponentForm type="text" width="w-[80%]" />
+              <p>SOL:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="para.sol"
+              >
+                {["", "1A", "2A", "3A", "1B", "2B", "3B", "DISC", "ALA G"].map(
+                  (e) => {
+                    return (
+                      <option
+                        className={`${styles["style-opt"]}`}
+                        key={e}
+                        value={e}
+                      >
+                        {e === "" ? "Selecione uma opção..." : e}
+                      </option>
+                    );
+                  },
+                )}
+              </SelectComponentForm>
             </div>
             <div className="flex justify-center gap-2">
-                <p>CELA:</p>
-              <SelectComponentForm type="text" width="w-[80%]" />
+              <p>CELA:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="para.cela"
+              >
+                {["", "1", "2", "3", "4", "5", "6"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
+                })}
+              </SelectComponentForm>
+            </div>
+          </fieldset>
+        </div>
+      </section>
+      <h2 className="text-center bg-cinza-escuro rounded-[5px] text-amarelo-claro text-[1.3em] font-bold">
+        Pedido de Seguro:
+      </h2>
+      <section className={`${styles["style-div"]}`}>
+        <div className="flex">
+          <p>Nome: </p>
+          <InputComponentForm type="text" width="w-full" />
+        </div>
+        <div className="flex justify-between gap-3">
+          <fieldset className="flex flex-col grow gap-3 border border-amarelo-claro rounded-[5px] p-2">
+            <legend className=" text-amarelo-claro text-center">De:</legend>
+            <div className="flex justify-center gap-2">
+              <p>PAV:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="de.pav"
+              >
+                {["", "I", "II"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
+                })}
+              </SelectComponentForm>
+            </div>
+            <div className="flex justify-center gap-2">
+              <p>SOL:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="de.sol"
+              >
+                {["", "1A", "2A", "3A", "1B", "2B", "3B", "DISC", "ALA G"].map(
+                  (e) => {
+                    return (
+                      <option
+                        className={`${styles["style-opt"]}`}
+                        key={e}
+                        value={e}
+                      >
+                        {e === "" ? "Selecione uma opção..." : e}
+                      </option>
+                    );
+                  },
+                )}
+              </SelectComponentForm>
+            </div>
+            <div className="flex justify-center gap-2">
+              <p>CELA:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="de.cela"
+              >
+                {["", "1", "2", "3", "4", "5", "6"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
+                })}
+              </SelectComponentForm>
+            </div>
+          </fieldset>
+          <fieldset className="flex flex-col grow gap-3 border border-amarelo-claro rounded-[5px] p-2">
+            <legend className=" text-amarelo-claro text-center">Para:</legend>
+            <div className="flex justify-center gap-2">
+              <p>PAV:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="para.pav"
+              >
+                {["", "I", "II"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
+                })}
+              </SelectComponentForm>
+            </div>
+            <div className="flex justify-center gap-2">
+              <p>SOL:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="para.sol"
+              >
+                {["", "1A", "2A", "3A", "1B", "2B", "3B", "DISC", "ALA G"].map(
+                  (e) => {
+                    return (
+                      <option
+                        className={`${styles["style-opt"]}`}
+                        key={e}
+                        value={e}
+                      >
+                        {e === "" ? "Selecione uma opção..." : e}
+                      </option>
+                    );
+                  },
+                )}
+              </SelectComponentForm>
+            </div>
+            <div className="flex justify-center gap-2">
+              <p>CELA:</p>
+              <SelectComponentForm
+                type="text"
+                width="w-[80%]"
+                onChange={handleChange}
+                name="para.cela"
+              >
+                {["", "1", "2", "3", "4", "5", "6"].map((e) => {
+                  return (
+                    <option
+                      className={`${styles["style-opt"]}`}
+                      key={e}
+                      value={e}
+                    >
+                      {e === "" ? "Selecione uma opção..." : e}
+                    </option>
+                  );
+                })}
+              </SelectComponentForm>
             </div>
           </fieldset>
         </div>
--- src/app/components/select/Select.tsx
+++ src/app/components/select/Select.tsx
@@ -1,7 +1,7 @@
 
-export default function SelectComponent({children, width, height, state}: any) {
+export default function SelectComponent({children, width, height, name, value, onChange}: any) {
 
-    return (<select name="" id="" className={`${width} ${height} text-sm cursor-pointer border border-amarelo-escuro rounded-[5px] outline-none `} onChange={(e) => {state(e.target.value)}} >
+    return (<select name={name} value={value} id="" className={`${width} ${height} text-sm cursor-pointer border border-amarelo-escuro rounded-[5px] outline-none `} onChange={onChange} >
                 {children}
             </select>)
 }
